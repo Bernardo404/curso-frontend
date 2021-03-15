58e1e920b1f88a756a19c23759c817c38d92dc7e
Bernardo404 <bernardotolentino14@gmail.com>
2021-03-15 03:18:10 -0300

    Aula java 3-Array

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-//Object
-//acesso as props com ponto
-const user = {
-    nome: 'Usuário',
-    idade: 25,
-    endereco: {
-      rua: 'Rua central',
-      numero: 1
-    },
-    projetos: ['Projeto 1', 'Projeto 2']
-  }  
-  console.log(user.nome)
-  console.log(user.endereco.rua)
-  console.log(user.projetos[1])
+const students = [
+    {id: 1, name: 'John Doe', age: 23, scholarship: false},
+    {id: 2, name: 'Margoe Rose', age: 19, scholarship: true},
+    {id: 3, name: 'Kayle', age: 22, scholarship: true}
+]
+var bolsistas=[" "," "];
+var cont=0;
+students.forEach(function(elem,x,students){
+  let nome=students[x]['name'];
+  let bolsistas= ["",""];
+  console.log(nome)
+  if(students[x]['age']>20){
+    console.log(students[x]['name']);
+  }
+  if(students[x]['scholarship']==true){
+    bolsistas[cont]=nome;
+    cont++;
+    console.log(bolsistas[cont-1])
+  }
+});
 
-  //acesso as props com colchetes
-  console.log(user['nome'])
-  console.log(user.endereco['rua'])
-  console.log(user.projetos[1])
+
